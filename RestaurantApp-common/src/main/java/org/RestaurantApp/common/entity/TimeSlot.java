3788d3a81844ab7cf753f48e8646eb2068b87643
komal-ingale <kingale@uncc.edu>
2016-07-18 19:25:50 -0400

    TimeSlot Id

--- RestaurantApp-common/src/main/java/org/RestaurantApp/common/entity/TimeSlot.java
+++ RestaurantApp-common/src/main/java/org/RestaurantApp/common/entity/TimeSlot.java
@@ -3,6 +3,8 @@ package org.RestaurantApp.common.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -15,6 +17,10 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "timeslot")
 public class TimeSlot {
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
 
 	@Column(name = "start_time")
 	private String startTime;
