63b84ddc7324b37bc32e683ef0840df225c7b737
praks3989 <prakashpatel3989@gmail.com>
2016-07-21 21:19:07 -0400

    Added StartUp class

---
+++ RestaurantApp-data-service/src/main/java/org/ResatuarantApp/data/service/controller/UserController.java
@@ -0,0 +1,43 @@
+package org.ResatuarantApp.data.service.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+	@RequestMapping(value = "/createUser", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+
+	public @ResponseBody String createUser(@RequestBody String test) throws Exception {
+
+		return test;
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+	// @RequestMapping(value = "/", method = RequestMethod.POST)
+	// public String printWelcome(ModelMap model) {
+	//
+	// model.addAttribute("message", "Spring 3 MVC Hello World");
+	// return "hello";
+	//
+	// }
+	//
+	// @RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+	// public ModelAndView hello(@PathVariable("name") String name) {
+	//
+	// ModelAndView model = new ModelAndView();
+	// model.setViewName("hello");
+	// model.addObject("msg", name);
+	//
+	// return model;
+	//
+	// }
+
+}
