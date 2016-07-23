190172f99ba1fd1030b6502de3f4d99824a42e36
praks3989 <prakashpatel3989@gmail.com>
2016-07-23 13:23:02 -0400

    Data-Persistent module added

---
+++ RestaurantApp-data-persistent-layer/src/main/java/org/RestaurantApp/data/persistent/layer/App.java
@@ -0,0 +1,13 @@
+package org.RestaurantApp.data.persistent.layer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
