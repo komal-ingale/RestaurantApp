e71f2c19939cfa49ef9c55b9d3126c72a83aab9a
praks3989 <prakashpatel3989@gmail.com>
2016-07-23 20:21:57 -0400

    restaurant controller

---
+++ RestaurantApp-data-service/src/main/java/org/ResatuarantApp/data/service/controller/RestaurantController.java
@@ -0,0 +1,30 @@
+package org.ResatuarantApp.data.service.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.RestaurantApp.common.entity.Restaurant;
+import org.RestaurantApp.data.persistent.layer.service.Restaurantservice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public @ResponseBody List<Restaurant> getRestaurant() {
+		List<Restaurant> allRestaurant = new ArrayList<Restaurant>();
+		try {
+			allRestaurant = Restaurantservice.getInstance().getAllRestaurants();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return allRestaurant;
+
+	}
+
+}
